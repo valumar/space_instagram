d7c3dd5b0c00bf49a29b04ec105c5e4489628209
Petr E. Antonov <antpeter@gmail.com>
2019-04-29 16:47:59 +0300

    Changing the method of extension  detection

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -1,4 +1,6 @@
 import requests
+import os.path
+
 import my_funcs
 
 
@@ -16,7 +18,7 @@ def fetch_hubble_image(image_id, collection):
     url = f'http://hubblesite.org/api/v3/image/{image_id}'
     response = requests.get(url)
     image_url = response.json()['image_files'][-1]['file_url']
-    my_funcs.get_image(image_url, 'hubble', f'hubble-{collection}-{image_id}.{my_funcs.get_image_extension(image_url)}')
+    my_funcs.get_image(image_url, 'hubble', f'hubble-{collection}-{image_id}{os.path.splitext(image_url)[-1]}')
 
 
 def get_image_ids_from_hubble_collection(collection):
